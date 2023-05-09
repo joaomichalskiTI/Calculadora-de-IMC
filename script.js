0a2be9516ffd660ee3157b16dd216d3821b3add9
João Pedro Vogel Michalski <joaomichalskiti@gmail.com>
2023-05-09 15:26:08 -0300

    .

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const resultado = document.getElementById('resul-imc');
 const alerta = document.getElementById('frase');
 
 
+function renderizar() {
+    caixa.style.display = "block";
+    resultado.innerHTML = imc1; 
+
+}
+
 function calcular() {
     const peso = parseInt(document.getElementById('peso').value);
     const altura = parseFloat(document.getElementById('altura').value);
@@ -13,34 +19,29 @@ function calcular() {
     const imc1 = (imc*10000).toFixed(2);
     
     if(imc1 <= 18.5) {
-        caixa.style.display = "block";
-        resultado.innerHTML = imc1
         alerta.innerHTML = "Cuidado! </br>Voce está abaixo do peso!"
+        renderizar();
 
     } else if(imc1 > 18.5 && imc1 <= 25) {
-        caixa.style.display = "block";
-        resultado.innerHTML = imc1
         alerta.innerHTML = "Voce está no peso ideal!"
+        renderizar();
 
     } else if(imc1 > 25 && imc1 <= 30) {
-        caixa.style.display = "block";
-        resultado.innerHTML = imc1
         alerta.innerHTML = "Cuidado! </br>Voce está com sobrepeso!"
+         renderizar();
 
     } else if(imc1 > 30  && imc1 <= 35) {
-        caixa.style.display = "block";
-        resultado.innerHTML = imc1
         alerta.innerHTML = "Cuidado! </br>Voce está com </br>obesidade moderada! ."
+        renderizar();
 
     } else if(imc1 > 35 && imc1 <= 40) {
-        caixa.style.display = "block";
-        resultado.innerHTML = imc1
         alerta.innerHTML = "Cuidado! </br>Voce está com </br>obesidade severa!"
+       renderizar();
 
     } else if(imc1 > 40) {
-        caixa.style.display = "block";
-        resultado.innerHTML = imc1
         alerta.innerHTML = "Cuidado! </br>Voce está com </br>obesidade mórbida!"
+        renderizar();
+
     }
 }   
 
@@ -50,7 +51,6 @@ var aparecerImc = document.querySelector(".imcCalculo");
 var aparecerCal = document.querySelector(".calorias-calculo");
 let butonIMC = document.querySelector(".clc-imc");
 let butonCalorias = document.querySelector(".clc-cal");
-
 
 function imc() {
     aparecerImc.style.display = "block";
@@ -69,6 +69,41 @@ function calorias() {
 /*função para calcular as calorias*/ 
 
 function calculate() {
-    const result = document.getElementById('result');
+
+
+    var sexo = document.getElementsByName('radsex');
+    let idadeCalorias = parseInt(document.getElementById('idade-calorias').value);
+    let alturaCalorias = parseFloat(document.getElementById('altura-calorias').value);
+    let kgCalorias = parseInt(document.getElementById('kg-calorias').value);
+    const resulCalorias = document.getElementById('result-calorias');
     const campo = document.querySelector('.caixaResultado');
+
+    if (isNaN(idadeCalorias) || isNaN(alturaCalorias) || isNaN(kgCalorias)) {
+        alert("Reveja os dados inseridos");
+    }  else {
+        const mascCalorias = (88.36 + (13.4 * kgCalorias) + (4.8 * alturaCalorias) - (5.7 * idadeCalorias));
+        const femCalorias = (447.6 + (9.2 * kgCalorias) + (3.1 * alturaCalorias) - (4.3 * idadeCalorias));
+        const mascCalorias1 = mascCalorias.toFixed(2);
+        const femCalorias1 = femCalorias.toFixed(2);
+
+        if(sexo[0].checked) {
+            campo.style.display = "block"
+            resulCalorias.innerHTML = `Voce gasta em média ${mascCalorias1}kcal por dia`
+        } else {
+            campo.style.display = "block"
+            resulCalorias.innerHTML = `Voce gasta em média ${femCalorias1}kcal por dia`
+        }
+    }
 }
+
+
+
+/* calculando calorias diárias
+Homens:
+ 88,36 + (13,4 x peso em kg) + (4,8 x altura em cm) - (5,7 x idade em anos)
+  88.36 + (13.4 * kgCalorias) + (4.8 * alturaCalorias) - (5.7 * idadeCalorias)
+Mulheres:
+ 447,6 + (9,2 x peso em kg) + (3,1 x altura em cm) - (4,3 x idade em anos) 
+      447.6 + (9.2 * kgCalorias) + (3.1 * alturaCalorias) - (4.3 * idadeCalorias)
+
+ */
